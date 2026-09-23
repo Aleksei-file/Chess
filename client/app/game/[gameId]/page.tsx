41cb339f9d2e5c5ffb5852aef9e4619cb7641950
Aleksei <aldm.cherepanov@gmail.com>
2026-09-23 15:05:07 +0100

    Playable chessboard

--- client/app/game/[gameId]/page.tsx
+++ client/app/game/[gameId]/page.tsx
@@ -2,7 +2,18 @@
  * Displays the active chess game for a specific saved match ID.
  */
 import type { JSX } from 'react';
+import GameBoard from '@/components/GameBoard';
 
-export default function GamePage(): JSX.Element {
-  return <div></div>;
+export default async function GamePage({
+  params,
+}: {
+  params: Promise<{ gameId: string }>;
+}): Promise<JSX.Element> {
+  const { gameId } = await params;
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <div>{`Game page for ${gameId}`}</div>
+      <GameBoard />
+    </div>
+  );
 }
--- client/components/GameBoard.tsx
+++ client/components/GameBoard.tsx
@@ -3,13 +3,54 @@
  */
 'use client';
 
-import type { JSX } from 'react';
-import { Chessboard } from 'react-chessboard';
+import { useRef, useState, type JSX } from 'react';
+import { Board } from '@/components/ui/Board';
+import { Chess } from 'chess.js';
 
-export function GameBoard(): JSX.Element {
+export default function GameBoard(): JSX.Element {
+  const chessGameRef = useRef(new Chess());
+  const chessGame = chessGameRef.current;
+  const [chessPosition, setChessPosition] = useState(chessGame.fen());
+
+  // TODO: Implement a more sophisticated AI for the opponent instead of random moves.
+  const makeRandomMove = (): void => {
+    const possibleMoves = chessGame.moves();
+    if (chessGame.isGameOver()) {
+      return;
+    }
+    const randomMove =
+      possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+    chessGame.move(randomMove);
+    setChessPosition(chessGame.fen());
+  };
+
+  const onPieceDrop = ({ sourceSquare, targetSquare }: any): boolean => {
+    if (!targetSquare) {
+      return false;
+    }
+    try {
+      chessGame.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: 'q',
+      });
+      setChessPosition(chessGame.fen());
+      setTimeout(makeRandomMove, 500);
+      return true;
+    } catch {
+      return false;
+    }
+  };
+
+  const chessboardOptions = {
+    position: chessPosition,
+    onPieceDrop,
+    id: 'play-vs-random',
+  };
+
   return (
-    <div>
-      <Chessboard />
+    <div className="w-full sm:max-w-[480px] md:max-w-[560px] lg:max-w-[640px]">
+      <Board options={chessboardOptions} />
     </div>
   );
 }
